96cc9365160dd1ad0667323de604014d4c60e246
rsoiffer <>
2022-04-03 03:23:14 -0400

    Added UI for train timer, objectives, inventory

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -5,6 +5,8 @@ public class CameraFollow : MonoBehaviour
     public Transform toFollow;
     public float followRateX = 0.1f;
     public float followRateY = 0.1f;
+    public float noiseX = 0.1f;
+    public float noiseY = 0.1f;
 
     private void FixedUpdate()
     {
@@ -12,5 +14,11 @@ public class CameraFollow : MonoBehaviour
             Mathf.Lerp(transform.position.x, toFollow.position.x, followRateX),
             Mathf.Lerp(transform.position.y, toFollow.position.y, followRateY),
             transform.position.z);
+
+        transform.position += new Vector3(
+            Random.Range(-1, 1) * noiseX,
+            Random.Range(-1, 1) * noiseY,
+            0
+        );
     }
 }
