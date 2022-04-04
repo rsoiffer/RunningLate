b9dee94d08381101832a082c0557a43477c495b6
rsoiffer <>
2022-04-04 04:25:33 -0400

    Added grass spawning

---
+++ Assets/Code/SpawnScenery.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SpawnScenery : MonoBehaviour
+{
+    public GameObject spawnPrefab;
+    public int numToSpawn;
+    public Vector2 spawnArea;
+
+    private void Start()
+    {
+        for (int i = 0; i < numToSpawn; i++)
+        {
+            var newPos = new Vector3(
+                Random.Range(-spawnArea.x, spawnArea.x),
+                Random.Range(-spawnArea.y, spawnArea.y),
+                0);
+            var newSpawn = Instantiate(spawnPrefab, transform);
+            newSpawn.transform.position += newPos;
+        }
+    }
+}
